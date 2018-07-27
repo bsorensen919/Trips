c2f575a9e0bb695d2969254104da7ed451755157
Beth Sorensen <beth.sorensen@bethsorensenmbp.infinitecampus.com>
2018-07-27 09:40:35 -0500

    File organization to have seperate files for my game

---
+++ resources/javascript/games.js
@@ -0,0 +1,54 @@
+function BuildADeck() {
+    let deck = [];
+  
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    
+    for (i = 0; i < 4; i++) {
+      // Start with the Ace
+      var card = {name: ("Ace of " +suits[i]), value: 1, ace: true}
+      deck.push(card);
+      
+      // Take care of the number cards
+      for (cardCounter = 2; cardCounter < 11; cardCounter++) {
+        card = {name: (cardCounter + " " + suits[i]), value: cardCounter, ace: false};
+        deck.push(card);
+      }
+      
+      // Finish up with the face cards
+      card = {name: ("Jack of " + suits[i]), value: 10, ace: false};
+      deck.push(card);
+      card = {name: ("Queen of " + suits[i]), value: 10, ace: false};
+      deck.push(card);
+      card = {name: ("King of " + suits[i]), value: 10, ace: false};
+      deck.push(card);
+    }
+    //console.log(deck);
+    return deck;
+  }
+  
+  function PickACard() {
+    return Math.floor(Math.random() * 52);
+  }
+  
+  function BlackJack() {
+  
+    let deck = BuildADeck();
+  
+    // Do some minimal error checking, if something went wrong don't continue with the game
+    if (deck.length != 52) {
+      console.log("Opps!!!! - Error")
+    } else {
+      // Pick two random cards
+      let card1 = PickACard();
+      let card2 = PickACard();
+  
+      let playerCards = [deck[card1], deck[card2]];
+          
+      console.log("Welcome to Blackjack!");
+      
+      console.log("You are dealt: ");
+      console.log(" " + playerCards[0].name);
+      console.log(" " + playerCards[1].name);
+    }
+  }
+  
--- resources/javascript/trips.js
+++ resources/javascript/trips.js
@@ -1,42 +1,4 @@
-console.log('hello');
-
 function ShowDate() {
     document.getElementById('date').innerHTML = Date();
 }
 
-function BlackJack() {
-
-let deck = [];
-
-let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-
-for (i = 0; i < 4; i++) {
-  // Start with the Ace
-  deck.push("Ace of " + suits[i]);
-  
-  // Take care of the number cards
-  for (card = 2; card < 11; card++) {
-    deck.push(card + " " + suits[i]);
-  }
-  
-  // Finish up with the face cards
-  deck.push("Jack of " + suits[i]);
-  deck.push("Queen of " + suits[i]);
-  deck.push("King of " + suits[i]);
-}
-
-// Pick two random cards
-let card1 = Math.floor(Math.random() * 52);
-let card2 = Math.floor(Math.random() * 52);
-let playerCards = [deck[card1], deck[card2]];
-
-
-console.log("Welcome to Blackjack!");
-console.log("The deck has " + deck.length + " cards in it.");
-console.log(deck);
-
-console.log("You are dealt: ");
-console.log(" " + playerCards[0]);
-console.log(" " + playerCards[1]);
-
-}
