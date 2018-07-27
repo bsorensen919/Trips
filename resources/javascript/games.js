cfb841ec98822bbdd043d1aebeb1c9209347ce7d
Beth Sorensen <beth.sorensen@bethsorensenmbp.infinitecampus.com>
2018-07-27 09:55:57 -0500

    More refactoring on my BuildADeck function

--- resources/javascript/games.js
+++ resources/javascript/games.js
@@ -2,30 +2,22 @@ function BuildADeck() {
     let deck = [];
   
     let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    let cardStrs = ['Ace', 'One', 'Two', 'Three', 'Four', 
+    let cardStrs = ['Ace', 'Two', 'Three', 'Four', 
                     'Five', 'Six', 'Seven', 'Eight', 'Nine', 
                     'Ten', 'Jack', 'Queen', 'King'];
     
-    for (i = 0; i < 4; i++) {
-      // Start with the Ace
-      var card = {name: ("Ace of " +suits[i]), value: 1, ace: true}
-      deck.push(card);
-      
+    for (i = 0; i < suits.length; i++) {
+
       // Take care of the number cards
-      for (let cardCounter = 2; cardCounter < 14; cardCounter++) {
-        card = {name: (cardStrs[cardCounter] + " of " + suits[i]), value: cardCounter, ace: false};
+      for (let cardCounter = 0; cardCounter < cardStrs.length; cardCounter++) {
+        card = {name: (cardStrs[cardCounter] + " of " + suits[i]), 
+                value: cardCounter, 
+                ace: cardCounter == 0 ? true : false};
         deck.push(card);
       }
       
-      // Finish up with the face cards
-      //card = {name: ("Jack of " + suits[i]), value: 10, ace: false};
-      //deck.push(card);
-      //card = {name: ("Queen of " + suits[i]), value: 10, ace: false};
-      //deck.push(card);
-      //card = {name: ("King of " + suits[i]), value: 10, ace: false};
-      //deck.push(card);
     }
-    // console.log(deck);
+    //console.log(deck);
     return deck;
   }
   
@@ -49,7 +41,6 @@ function BuildADeck() {
       let playerCards = [deck[card1], deck[card2]];
           
       console.log("Welcome to Blackjack!");
-      
       console.log("You are dealt: ");
       console.log(" " + playerCards[0].name);
       console.log(" " + playerCards[1].name);
