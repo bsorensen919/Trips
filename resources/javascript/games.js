b22852ae01e0dcec8be5910cd038ec6a38b2d4e7
Beth Sorensen <beth.sorensen@bethsorensenmbp.infinitecampus.com>
2018-07-30 10:07:59 -0500

    Added a handler for the Hit Button

--- resources/javascript/games.js
+++ resources/javascript/games.js
@@ -6,10 +6,12 @@ let stayButton = document.getElementById("stay-button");
 hitButton.style.display = "none";
 stayButton.style.display = "none";
 
+let gDeck = [];
+let gPlayerCards = [];
 
-
-function BuildADeck() {
-    let deck = [];
+function BuildANewDeck() {
+    // Make sure the deck is empty before filling it up
+    gDeck = [];
   
     let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
     let cardStrs = ['Ace', 'Two', 'Three', 'Four', 
@@ -21,17 +23,16 @@ function BuildADeck() {
       // Take care of the number cards
       for (let cardCounter = 0; cardCounter < cardStrs.length; cardCounter++) {
         card = {name: (cardStrs[cardCounter] + " of " + suits[i]), 
-                value: cardCounter, 
-                ace: cardCounter == 0 ? true : false};
-        deck.push(card);
+                value: cardCounter + 1, 
+                ace: cardCounter == 1 ? true : false};
+        gDeck.push(card);
       }
     }
     //console.log(deck);
-    return deck;
 }
   
 function pickCard() {
-  return Math.floor(Math.random() * 52);
+  return Math.floor(Math.random() * gDeck.length);
 }
 
 newGameButton.addEventListener('click', function() {
@@ -41,11 +42,12 @@ newGameButton.addEventListener('click', function() {
   hitButton.style.display = "inline";
   stayButton.style.display = "inline";
   
-  let deck = BuildADeck();
+  // Update the global deck so that it has 52 cards again
+  BuildANewDeck();
   
   // Do some minimal error checking, if something went wrong don't continue with the game
-  if (deck.length != 52) {
-    console.log("Opps!!!! - Error Length = " + deck.length);
+  if (gDeck.length != 52) {
+    console.log("Opps!!!! - Error Length = " + gDeck.length);
 
   } else {
     // Pick two random cards
@@ -53,20 +55,33 @@ newGameButton.addEventListener('click', function() {
     let card1 = pickCard();
     let card2 = pickCard();
       
-    let playerCards = [deck[card1], deck[card2]];
+    gPlayerCards.push(gDeck[card1]);
+    gPlayerCards.push(gDeck[card2]);
 
-    deck.splice(card1, 1);
-    deck.splice(card2, 1);
+    gDeck.splice(card1, 1);
+    gDeck.splice(card2, 1);
 
-    console.log(deck);
+    console.log(gDeck);
         
     console.log("Welcome to Blackjack!");
     console.log("You are dealt: ");
-    console.log(" " + playerCards[0].name);
-    console.log(" " + playerCards[1].name);
+    console.log(" " + gPlayerCards[0].name);
+    console.log(" " + gPlayerCards[1].name);
 
-    console.log("There are %d cards left in the deck", deck.length);
+    console.log("There are %d cards left in the deck", gDeck.length);
   }
+
+});
+
+hitButton.addEventListener('click', function() {
+  let newCard = pickCard();
+
+  console.log(newCard);
+  console.log(gDeck[newCard]);
+
+  gPlayerCards.push(gDeck[newCard]);
+
+  gDeck.splice(newCard, 1);
 
 });
   
