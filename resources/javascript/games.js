8296879dfd9a971166e091a485e029464a235825
Beth Sorensen <beth.sorensen@bethsorensenmbp.infinitecampus.com>
2018-07-30 09:45:16 -0500

    First step of hooking up a UI

--- resources/javascript/games.js
+++ resources/javascript/games.js
@@ -1,3 +1,13 @@
+let textArea = document.getElementById("text-area");
+let newGameButton = document.getElementById("new-game-button");
+let hitButton = document.getElementById("hit-button");
+let stayButton = document.getElementById("stay-button");
+
+hitButton.style.display = "none";
+stayButton.style.display = "none";
+
+
+
 function BuildADeck() {
     let deck = [];
   
@@ -15,43 +25,48 @@ function BuildADeck() {
                 ace: cardCounter == 0 ? true : false};
         deck.push(card);
       }
-      
     }
     //console.log(deck);
     return deck;
-  }
+}
   
-  function pickCard() {
-    return Math.floor(Math.random() * 52);
-  }
-  
-  function BlackJack() {
-  
-    let deck = BuildADeck();
-  
-    // Do some minimal error checking, if something went wrong don't continue with the game
-    if (deck.length != 52) {
-      console.log("Opps!!!! - Error Length = " + deck.length);
+function pickCard() {
+  return Math.floor(Math.random() * 52);
+}
 
-    } else {
-      // Pick two random cards
+newGameButton.addEventListener('click', function() {
+  
+  textArea = "Started...";
+  newGameButton.style.display = "none"
+  hitButton.style.display = "inline";
+  stayButton.style.display = "inline";
+  
+  let deck = BuildADeck();
+  
+  // Do some minimal error checking, if something went wrong don't continue with the game
+  if (deck.length != 52) {
+    console.log("Opps!!!! - Error Length = " + deck.length);
 
-      let card1 = pickCard();
-      let card2 = pickCard();
+  } else {
+    // Pick two random cards
+
+    let card1 = pickCard();
+    let card2 = pickCard();
+      
+    let playerCards = [deck[card1], deck[card2]];
+
+    deck.splice(card1, 1);
+    deck.splice(card2, 1);
+
+    console.log(deck);
         
-      let playerCards = [deck[card1], deck[card2]];
+    console.log("Welcome to Blackjack!");
+    console.log("You are dealt: ");
+    console.log(" " + playerCards[0].name);
+    console.log(" " + playerCards[1].name);
 
-      deck.splice(card1, 1);
-      deck.splice(card2, 1);
-
-      console.log(deck);
-          
-      console.log("Welcome to Blackjack!");
-      console.log("You are dealt: ");
-      console.log(" " + playerCards[0].name);
-      console.log(" " + playerCards[1].name);
-
-      console.log("There are %d cards left in the deck", deck.length);
-    }
+    console.log("There are %d cards left in the deck", deck.length);
   }
+
+});
   
